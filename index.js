d40c3a07f80b600af5119eb8b0c45ff58b0b42e8
eileenghm <guoh@brandeis.edu>
2023-08-22 15:28:43 -0700

    make sound

--- index.js
+++ index.js
@@ -1,6 +1,80 @@
 //add listen to button, and handle click
-document.querySelector("button").addEventListener("click", handleClick);
+//no () for the function, so it can be triggered later
+//we pass the name of function instead of executing the function
+
+
+
+let drumLists = document.querySelectorAll("button");
+
+for (let i = 0; i < drumLists.length; i++) {
+    drumLists[i].addEventListener("click", handleClick);
+}
 
 function handleClick() {
-
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
 }
+
+function makeSound(key) {
+
+    switch (key) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+  
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+  
+      case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+  
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+  
+      case "j":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+  
+      case "k":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+  
+      case "l":
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+  
+  
+      default: console.log(key);
+    }
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+  
+    activeButton.classList.add("pressed");
+  
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+  
+}
+
+document.addEventListener("keypress", function(event) {
+
+    makeSound(event.key);
+  
+    buttonAnimation(event.key);
+  
+  });
